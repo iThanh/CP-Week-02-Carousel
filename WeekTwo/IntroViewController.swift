1c1a159ad4adfed1cd7f0426c9d381529380c6c3
Jan Senderek <jan@jan-mbp.corp.dropbox.com>
2016-02-09 09:59:51 -0800

    Update

---
+++ WeekTwo/IntroViewController.swift
@@ -0,0 +1,61 @@
+//
+//  IntroViewController.swift
+//  WeekTwo
+//
+//  Created by Jan Senderek on 2/9/16.
+//  Copyright © 2016 Jan Senderek. All rights reserved.
+//
+
+import UIKit
+
+class IntroViewController: UIViewController {
+
+    
+    //Outlets
+
+    @IBOutlet weak var introScrollFeed: UIScrollView!
+    @IBOutlet weak var introScrollFeedImage: UIImageView!
+    
+    @IBOutlet weak var tileImage1: UIImageView!
+    
+    //Functions
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        introScrollFeed.contentSize = CGSizeMake(320, introScrollFeedImage.image!.size.height)
+        
+    
+        //Default Tile Rotation
+        tileImage1.transform = CGAffineTransformMakeDegreeRotation(-10)
+        
+        
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+    
+    @IBAction func rotateTile1(sender: AnyObject) {
+        print("Rotated 1")
+    }
+    
+    
+    
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
